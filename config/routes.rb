ba6e3e35a8bfad8ad206c00ea7ce77e6686df37e
tanikura <122531271+tanikura@users.noreply.github.com>
2023-02-27 12:43:03 +0000

    [Finish] 4-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get 'homes/about', to: 'homes#about', as: 'about'
+  match '/about', to: 'homes#about', via: 'get'
 end
