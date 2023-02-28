1896bd02000d87554bdaf3474f7cfdbdb2c57c2c
tanikura <122531271+tanikura@users.noreply.github.com>
2023-02-28 13:23:08 +0000

    [Add] 4-11途中

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,8 +1,9 @@
 class PostImagesController < ApplicationController
+
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -11,18 +12,17 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
   end
-  
-  def destroy
-  end
-  
+
+
   private
-  
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
-  
+
 end
