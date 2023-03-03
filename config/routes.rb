8a0711c424cdcc4b66245eda8f9fe7aa0d880a93
tanikura <122531271+tanikura@users.noreply.github.com>
2023-03-03 11:26:50 +0000

    [Finish] 4-13

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   devise_for :users
-  
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
-  get 'post_images/destroy'
 
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
-  get "/homes/about" => "homes#about", as: "about"
 end
